02537850eaf8b8eb4b60b03dac3cbb71e97e49f8
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-05 13:45:15 -0400

    Working but getting slightly wrong output.. time to troubleshoot

--- sales_tax/receipt.rb
+++ sales_tax/receipt.rb
@@ -13,15 +13,13 @@ attr_accessor :item
 
 
 	def total_sales_tax #add up taxes on each item in shopping_cart array
-		total_sales_tax = @shopping_cart.map { |n| n.tax }
-		total_sales_tax.inject{:+}
+		@shopping_cart.inject(0) { |sum, product| sum += product.tax }
 
 	end
 
 
 	def total_cost #what customer pays
-		total_cost = @shopping_cart.map { |n| n.total_for_item }
-		total_cost.inject{:+}
+	@shopping_cart.inject(0) { |sum, product| sum += product.total_for_item }
 	end
 
 
@@ -29,14 +27,22 @@ attr_accessor :item
 		@shopping_cart.each do |product|
 			puts "#{product.name} --> #{product.total_for_item}"
 		end #end of each do
-		puts "Total Sales Tax: #{total_sales_tax}"
-		puts "Total Cost: #{total_cost}"
+		puts "#{total_sales_tax}"
+		puts "#{total_cost}"
 
 	end #end of output method
 
 end # end of Receipt class
 
-#shopping_cart_1
+
+
+
+###################################
+#### Below is the Point Of Sale ###
+###################################
+
+### shopping_cart_1
+
 book = Item.new("Book", 12.49, "no_tax")
 cd = Item.new("CD", 14.99, "tax")
 chocolate = Item.new("Chocolate Bar", 0.85, "no_tax")
@@ -46,4 +52,19 @@ shopping_cart_1.add(book)
 shopping_cart_1.add(cd)
 shopping_cart_1.add(chocolate)
 
-shopping_cart_1.output
+### shopping_cart_2
+
+imp_chocolate = Item.new("Imported Chocolate", 10.50, "no_tax imported")
+imp_perfume = Item.new("Imported Perfume", 54.65, "tax imported")
+
+shopping_cart_2 = Receipt.new
+shopping_cart_2.add(imp_chocolate)
+shopping_cart_2.add(imp_perfume)
+
+
+
+#####  Output  #####
+puts "---shopping_cart_1---"
+puts shopping_cart_1.output
+puts "---shopping_cart_2---"
+puts shopping_cart_2.output
